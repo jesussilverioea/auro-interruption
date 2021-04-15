ac52c6c05f48252fb5e4a502722a5d3da31eb83a
Dale Sande <dale.sande@gmail.com>
2021-04-14 17:52:08 -0700

    fix: update dist for interruptionToggle function

--- demo/toggleInterruption.js
+++ demo/toggleInterruption.js
@@ -1,3 +1,12 @@
+// Copyright (c) Alaska Air. All right reserved. Licensed under the Apache-2.0 license
+// See LICENSE in the project root for license information.
+
+// ---------------------------------------------------------------------
+
+/**
+ * @param {string} elName ID for targeted element
+ * @returns {dom} attribute setting
+ */
 toggleInterruption = (elName) => {
   let dialog = document.querySelector(elName);
   const html = document.querySelector('html');
